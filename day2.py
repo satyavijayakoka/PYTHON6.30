c247d3afba940dc62489c17fbe18dac87bebd41a
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-07 10:27:31 -0500

    day2

---
+++ day2.py
@@ -0,0 +1,85 @@
+# introvert    # extrovert
+# calm           loud
+# less outing    more outing
+# less social    more social
+# Human - 
+# Properties - weight , height , color 
+# Methods   - walk() , talk()
+
+# Vehicle 
+# Properties -  color , regNo , type , model 
+# method     - start() , stop()
+
+# Bank acc
+# Propeties - accNo , accType , bal , IFSC
+# Method - deposit() , withdrawl()
+
+# program 1
+a=10
+print(a)
+print(type(a))
+
+b=True
+print(b)
+print(type(b))
+
+c="satya"
+print(c)
+print(type(c))
+
+d=12.3
+print(d)
+print(type(d))
+
+# program 2
+# comparison operators
+#<,>,<=,>=,==,!=
+print(6 > 3)
+print(6 < 3)
+print(6 >= 3)
+print(6 <= 3)
+print(6 >= 6)
+print(6 <= 6)
+print(6 != 6)
+print(7 == 7)
+
+# logical operators
+# and, or , not
+
+# and
+print(5 == 5 and 6 == 6)
+print(7 == 8 and 9 == 9)
+print(6 == 6 and 8 == 9)
+print(8 == 9 and 9 == 8)
+
+#or
+print(5 == 5 or 6 == 6)
+print(7 == 8 or 9 == 9)
+print(6 == 6 or 8 == 9)
+print(8 == 9 or 9 == 8)
+
+#not
+print(not(9==9))
+print(not(8==9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
