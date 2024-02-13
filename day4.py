a7c6057b7eacfdf7324bd5fed131e08fbebb97e3
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-13 11:53:02 -0500

    day4

---
+++ day4.py
@@ -0,0 +1,65 @@
+# loops ??
+# while and for loop
+
+#  for 
+# range()
+
+#for x in range(startIndex,EndIndex(not included),stepSize):
+    # statements
+
+# program 1
+# print 0 to 9
+for x in range(10): # # if startIndex not mentioned , zero is deafault
+    print(x)
+
+# print 2 to 9
+for x in range(2,10):
+    print(x)
+
+# print 2 to 5
+for x in range(2,6):
+    print(x)
+
+# print table of 2
+for x in range(2,21,2):
+    print(x)
+
+# print table of 5 in reverse
+for x in range(50,4,-5):
+    print(x)
+
+# print table of 10 in reverse
+for x in range(100,9,-10):
+    print(x)
+
+#break with for loop
+for x in range(1,6):
+    if x==3:
+        break
+    print(x)
+
+for x in range(1,6):
+    print(x)
+    if x==3:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
