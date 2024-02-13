a407efe9898af35c0ded6cffb6f6f4b37b0ea56f
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-13 12:13:51 -0500

    day5

---
+++ day5.py
@@ -0,0 +1,56 @@
+# # program 1
+# for x in range(1,11):
+#     print(x)
+
+# # program 2
+# for x in range(1,11):
+#     print(x*2)
+
+# # program 3
+# for x in range(5):
+#     print(x)
+
+# # program 4
+# for x in range(50,4,-5):
+#     print(x)
+
+# # program 5
+# for x in range(1,10):
+#     if x==3:
+#         break
+#     print(x)
+
+# # program 6
+# for x in range(1,5):
+#     print(x)
+#     if(x==3):
+#         break
+
+# # program 7
+# for x in range(1,6):
+#     if x==3:
+#         continue
+#     print(x)
+
+# # program 8
+# for x in range(6,1,-1):
+#     if x==3:
+#         continue
+#     print(x)
+
+# program 9
+for x in range(5,51,5):
+    if x==25:
+        continue
+    print(x)
+
+# program 10
+for x in range(30,2,-3):
+    if x==21:
+        continue
+    print(x)
+
+
+
+
+
