2c69c440daa7d76804ac72688deecd834f54d94a
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-14 12:41:29 -0500

    day6

---
+++ day6.py
@@ -0,0 +1,110 @@
+# loop 
+#for x in range(startIndex, endIndex(not included),stepSize):
+    # statements
+
+# for x in range(1,6):
+#     if x==3:
+#         break
+#     print(x)    
+# for x in range(2,21,+2):
+#     print(x)
+#     if x==10:
+#         break
+# for x in range(5,1,-1):
+#     if x==3:
+#         continue
+#     print(x)
+
+# while loop
+# intialization 
+# while(condition):
+    # statement
+    # increment
+# program 1 print 1 to 3
+t1=1
+while(t1<=3):
+    print(t1)
+    t1=t1+1
+
+# program 2 print 1 to 5
+t2=1
+while(t2<=5):
+    print(t2)
+    t2=t2+1
+
+# program 3 table of 2
+t3=2
+while(t3<=20):
+    print(t3)
+    t3=t3+2
+# program 4 table of 5
+t4=50
+while(t4>=5):
+    print(t4)
+    t4=t4-5
+# program 5 break statement
+t5=1
+while(t5<=5):
+    if t5==3:
+        break
+    print(t5)
+    t5=t5+1
+
+# program 6
+t6=5
+while(t6>=1):
+    if t6==3:
+        break
+    print(t6)
+    t6=t6-1
+
+# program 7 continue
+t7=1
+while(t7<=5):
+    if t7==3:
+        t7=t7+1#4
+        continue
+    print(t7)
+    t7=t7+1
+
+# program 8
+t8=5
+while(t8>=1):
+    if t8==2:
+        t8=t8-1
+        continue
+    print(t8)
+    t8=t8-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
