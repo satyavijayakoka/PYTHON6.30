c808492753cc19ab8f051cfadfef0d191be6f8b1
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-02-19 11:45:55 -0500

    day8

---
+++ day8.py
@@ -0,0 +1,67 @@
+names=["satya","koshika","jathin","sai"]
+names2= names
+names2[0]="ratnam"
+print(names)
+print(names2)
+
+# program 2 append()
+fruits=["apple","banana","grapes","mango"]
+fruits.append("orange")
+print(fruits)
+
+# program 3
+# pop() , pop(index)  , remove()
+#               0           1           2         3          4      5
+vegetables=["cabbage","cauliflower","carrot","capsicum","tomato","potato"]
+vegetables.pop()
+print(vegetables)
+vegetables.pop(2)
+print(vegetables)
+vegetables.remove("cauliflower")
+print(vegetables)
+
+# program 4
+animals=["tiger","lion","bear","panther","tiger"]
+# animals.clear()
+# print(animals)
+q1= animals.count("tiger")
+print(q1)
+
+# program 5
+city=["pune","hyderabad","bangalore","kolkatta"]
+city.reverse()
+print(city)
+
+# program 6
+country=["India","Bangladesh","Srilanka","Nepal"]
+country.insert(3,"China")
+print(country)
+
+# program 7
+district=["chandrapur","wardha","nagpur"]
+district.sort()
+print(district)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
