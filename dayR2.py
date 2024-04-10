97b89c84c2cbfc6d20e9c903705d0dcd3cd44f44
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-04-09 21:49:57 -0400

    added

---
+++ dayR2.py
@@ -0,0 +1,183 @@
+# loops -- for loop
+# program 1
+#range(startIndex,endIndex,stepSize)
+# by default startIndex is 0
+# for x in range(5):
+#     print(x)
+
+# # program 2
+# # endIndex not included
+# for x in range(1,7):
+#     print(x)
+
+# # program 3
+# for x in range(1,5,1):
+#     print(x)
+
+# # program 4
+# for x in range (2,21,2):
+#     print(x)
+
+# # program 5
+# for x in range(20,1,-2):
+#     print(x)
+
+# program 6
+# for x in range(50,4,-5):
+#     print(x)
+
+# # program 7
+# # break statement
+# for x in range(1,6):
+#     if x == 3:
+#         break
+#     print(x)
+
+# # program 8
+# for x in range(1,6):
+#     print(x)
+#     if x == 3:
+#         break
+
+# program 9
+# for x in range(2,21,2):
+#     if x == 8:
+#         break
+#     print(x)
+
+# # program 10
+# for x in range(20,1,-2):
+#     if x == 12:
+#         break
+#     print(x)
+
+# program 11
+# continue with for loop
+# for x in range(1,5,1):
+#     if x == 2:
+#         continue
+#     print(x)
+
+# # program 12
+# for x in range(50,4,-5):
+#     if x == 15:
+#         continue
+#     print(x)
+
+# intialization
+#while(condition):
+    # statement
+    # increment / decrement
+
+# program a
+# i1 = 1
+# while(i1 <= 3):
+#     print("hello")
+#     i1 = i1+1
+
+# # program b
+# i2 = 1
+# while(i2 <= 5):
+#     print(i2)
+#     i2 = i2+1
+
+# # program c
+# i3 = 5
+# while(i3 >=1):
+#     print(i3)
+#     i3 = i3-1
+
+# program d
+# i4 = 2
+# while(i4 <= 20):
+#     print(i4)
+#     i4 = i4+2
+
+# program e
+# i5 = 50
+# while(i5 >= 5):
+#     print(i5)
+#     i5 = i5-5
+
+# program f
+# break statement
+# i6 = 1
+# while(i6 <= 5):
+#     if i6 == 3:
+#         break
+#     print(i6)
+#     i6 = i6+1
+
+# program g
+# i7 = 5
+# while(i7 >= 1):
+#     if i7 == 3:
+#         break
+#     print(i7)
+#     i7 = i7-1
+
+# program h
+i8 = 6
+while(i8 >= 1):
+    print(i8)
+    if i8 == 4:
+        break
+    i8 = i8-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
