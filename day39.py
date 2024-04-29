78379955cfb25e2eb76d0f977f5f726c227bf3f4
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-04-29 11:37:42 -0400

    added

--- day39.py
+++ day39.py
@@ -36,9 +36,75 @@ str5 = "i am learning javascriet and javascriet"
 q2 = re.sub(r"javascriet","javascript",str5)
 print(q2)   # i am learning javascript and javascript
 
+# search()
+# findall()
+# match()
+# split()
+# sub()
+# group()
+
+# \w [a-z A-Z 0-9]
+# \W [not [a-z A-Z 0-9]]
+# \d - [0-9]
+# \D - not[0-9]
+# \b - blank space
+# \A - match only start of string 
+# \Z - match only at end 
+
+# program 1
+import re
+str = 'an apple a day keeps doctor away'
+#listA = re.findall(r'a[\w]*',str) #['an', 'apple', 'a', 'ay', 'away']
+listA = re.findall(r'\ba[\w]*',str)
+print(listA)
+
+# program 2
+import re
+str = 'The meeting will be conducted on 1st and 21st of this month'
+# 1st  21st
+
+# program 3
+# listB = re.findall(r'\b\d[\w]*',str)
+# print(listB) #['1st', '21st']
+
+listB = re.findall(r'\b\d[\d]*',str)
+print(listB) #['1', '21']
+
+# program 4
+import re
+str = "one two three four five six seven 8 9 10"
+listC = re.findall(r'\b\w{5}\b',str)
+print(listC) # ['three', 'seven']
+
+listC = re.search(r'\b\w{5}\b',str)
+print(listC.group()) #three
+
+# listC = re.match(r'\b\w{5}\b',str)
+# print(listC.group())
+
+# program 5
+str = "one two three four five six seven 8 9 10"
+listD = re.findall(r'\b\w{4,}\b',str)
+print(listD) # ['three', 'four', 'five', 'seven']
 
 
+str = "one two three four five six seven 8 9 10"
+listD = re.findall(r'\b\w{3,5}\b',str)
+print(listD)  # ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
 
+
+str = "one two three four five six seven 8 9 10"
+listD = re.findall(r'\b\d{1,}\b',str)
+print(listD)  # ['8', '9', '10']
+
+str = "one two three four five six seven 8 9 10"
+listD = re.findall(r't[\w]*\Z',str)
+print(listD) # []
+
+
+str = "one two three four five six seven 8 9 10 two"
+listD = re.findall(r'\At[\w]*',str)
+print(listD)  # []
 
 
 
